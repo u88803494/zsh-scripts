8fb00269b392f7f4f4ceaca38a9057c1bea378d7
Hugh Lee <u88803494@gmail.com>
2022-05-10 10:40:48 +0800

    Add clear and remove all branches except the main branch scripts

--- custom.plugin.zsh
+++ custom.plugin.zsh
@@ -1,2 +1,6 @@
 # Initialize the repository
 alias gsetpuser='git config user.name "Hugh Lee" && git config user.email "u88803494@gmail.com"'
+
+# misc
+alias cls='clear'
+alias gbdallbranch='git stash && gco main && git branch | grep -v "main" | xargs git branch -D'
