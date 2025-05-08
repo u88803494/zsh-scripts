95ca61d02d4609a81eabf4171241f1bdd67beb45
Henry Lee <u88803494@gmail.com>
2025-05-08 17:04:28 +0800

    append basic script

--- custom.plugin.zsh
+++ custom.plugin.zsh
@@ -1,6 +1,10 @@
 # Initialize the repository
-alias gsetpuser='git config user.name "Hugh Lee" && git config user.email "u88803494@gmail.com"'
+alias gsetuser='git config user.name "Henry Lee" && git config user.email "yuhao@hismax.onmicrosoft.com"'
 
 # misc
 alias cls='clear'
 alias gbdallbranch='git stash && gco main && git branch | grep -v "main" | xargs git branch -D'
+
+## Checkout
+alias gcp='git checkout production'
+alias gcs='git checkout staging'
